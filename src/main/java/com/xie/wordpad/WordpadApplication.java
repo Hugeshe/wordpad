3dbab2546c33e38730b37ec519182e3d74164545
Hugeshe <13340001050>
2020-01-29 16:51:19 +0800

    init

---
+++ src/main/java/com/xie/wordpad/WordpadApplication.java
@@ -0,0 +1,13 @@
+package com.xie.wordpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordpadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordpadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xie/wordpad/WordpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xie.wordpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordpadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
